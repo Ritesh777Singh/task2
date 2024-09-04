581563c1803f063f9359e0d71cefa2ade131c9ad
Ritesh777Singh <singhrt456@gmail.com>
2024-09-04 15:12:38 +0530

    Update Q18.c

--- Looping /Q18.c
+++ Looping /Q18.c
@@ -1,1 +1,20 @@
+/*18.Write a C Program to Print the Multiplication Table of N
+i. E.g. 5 * 1 = 5
+ii. 5 * 2 = 10
+1. .
+2. .
+iii. 5 * 10 = 50 */
+
+
+#include <stdio.h>
+int main(){
+    int num;
+    printf("Enter The Number :");
+    scanf("%d",&num);
+    for(int i = 1;i<=10;i++){
+        int mul;
+        mul = num * i;
+        printf(" Table  %d * %d = %d\n",num,i,mul);
+    }
+}
 
