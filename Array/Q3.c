8a2774df2f3a2fc0cbc6fc71fb5c307758a8e79c
Ritesh777Singh <singhrt456@gmail.com>
2024-09-05 14:03:47 +0530

    Update Q3.c

--- Array/Q3.c
+++ Array/Q3.c
@@ -1,1 +1,24 @@
+//3. WAP to find reverse of string using recursion
 
+#include <stdio.h>
+#include<string.h>
+void reverse(char str[],int start_idx,int end_idx){
+    if(start_idx>=end_idx){
+        return;
+    }
+    else{
+        char temp = str[start_idx];
+         str[start_idx] = str[end_idx];
+         str[end_idx] = temp;
+         reverse(str,start_idx+1,end_idx-1);
+    }
+}
+int main(){
+    char str[50];
+    printf("Enter String:");
+    fgets(str,sizeof(str),stdin);
+    int len = strlen(str);
+    reverse(str,0,len-1);
+    printf("%s",str);
+    return 0;
+}
