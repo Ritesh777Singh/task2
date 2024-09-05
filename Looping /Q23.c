088e6d1f62a34abfb0ada82c9b7c389d0463ef5e
Ritesh777Singh <singhrt456@gmail.com>
2024-09-05 12:04:52 +0530

    Update Q23.c

--- Looping /Q23.c
+++ Looping /Q23.c
@@ -1,1 +1,13 @@
+//23. C Program to Reverse a Number Using FOR Loop Series Program:
 
+#include <stdio.h>
+int main() {
+    int num, reverse = 0;
+        printf("Enter the number: ");
+        scanf("%d", &num);
+        for(;num != 0; num /= 10){
+        reverse = reverse * 10 + num % 10;
+       }
+       printf("The reversed number is: %d", reverse);
+       
+}
