be9c6018005c3993a1ea46528cb8b449981d4fcd
Ritesh777Singh <singhrt456@gmail.com>
2024-09-05 14:32:02 +0530

    Update Q8.c

--- Array/Q8.c
+++ Array/Q8.c
@@ -1,1 +1,37 @@
+/* 8. WAP to reverse a string and check that the string is palindrome or not
+Write a program of structure employee that provides the following
+a. information -print and display empno, empname, address
+andage
+b. Write a program of structure for five employee that
+provides the following information -print and display
+empno, empname, address andage */
 
+// WAP to reverse a string and check that the string is palindrome or not
+
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char str[50];
+    printf("Enter String: ");
+    scanf("%s",str);
+    int len = strlen(str);
+    // printf("%d\n",len);
+    char rev[50];
+    int j = 0;
+    for(int i = len-1 ; i >= 0;i--){
+        rev[j]=str[i];
+        j++;
+    }
+     rev[j] = '\0';
+    int rev_len = strlen(rev);
+    printf("Reverse : %s\n",rev);
+    
+    int result = strcmp(str, rev);
+    // printf("%d",result);
+    if(result == 0){
+         printf("Palindrom");
+    }
+    else{
+        printf("Not Palindrom");
+    }
+}
