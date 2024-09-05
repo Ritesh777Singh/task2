7556fb99c713e3f7596629c90e6bcb90b63b30fd
Ritesh777Singh <singhrt456@gmail.com>
2024-09-05 12:11:05 +0530

    Update Q24.c

--- Looping /Q24.c
+++ Looping /Q24.c
@@ -1,1 +1,16 @@
+//24. 1 + 2 + 3 + 4 + 5 + ... + n
 
+#include <stdio.h>
+int main() {
+    int num;
+    printf("Enter a number : ");
+    scanf("%d", &num);
+    for (int i = 1; i <= num; i++) {
+        if(i==num){
+            printf("%d ",i);
+        }
+        else{
+        printf("%d + ",i);
+        }
+    }
+}
