e187cb9723315b16b36397b13d7ff49fd6e81c2a
Ritesh777Singh <singhrt456@gmail.com>
2024-09-06 08:47:23 +0530

    Update Q13.c

--- Array/Q13.c
+++ Array/Q13.c
@@ -1,1 +1,18 @@
+//13.WAP to accept 5 numbers from user and check entered number is even or odd using of array
 
+#include <stdio.h>
+int main(){
+    int num[5];
+    for(int i = 0 ; i < 5 ; i++){
+        printf("Enter Number %d : ",i+1);
+        scanf("%d",&num[i]);
+    }
+    for(int i = 0; i<5 ; i++){
+        if(num[i]%2 == 0){
+            printf("Even : %d\n",num[i]);
+        }
+        else{
+            printf("Odd : %d\n",num[i]);
+        }
+    }
+}
