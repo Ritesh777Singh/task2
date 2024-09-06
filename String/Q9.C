fe60d73d7d764dcc67f8331211f5664c2cece00e
Ritesh777Singh <singhrt456@gmail.com>
2024-09-06 11:37:43 +0530

    Update Q9.C

--- String/Q9.C
+++ String/Q9.C
@@ -1,1 +1,13 @@
+// 9. Write a program in C to find the maximum number of characters in a string.
 
+#include <stdio.h>
+int main(){
+    char str[50];
+    printf("Enter String: ");
+    fgets(str,sizeof(str),stdin);
+    int ch = 0;
+    while(str[ch] != '\0' && str[ch] != '\n'){
+        ch++;
+    }
+    printf("The Maximum Number Of String :%d ",ch);
+}
