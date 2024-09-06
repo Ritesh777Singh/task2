5f434f6afbec0c688c60ba9ca1109cba65fa0400
Ritesh777Singh <singhrt456@gmail.com>
2024-09-06 09:06:40 +0530

    Update Q16.c

--- Array/Q16.c
+++ Array/Q16.c
@@ -1,1 +1,14 @@
+// 16.Accept 5 numbers from user and perform sum of array
+
+#include <stdio.h>
+int main(){
+    int num[5];
+    int sum = 0;
+    for(int i = 0;i<5;i++){
+        printf("enter number %d : ",i+1);
+        scanf("%d",&num[i]);
+        sum += num[i];
+    }
+   printf("sum of array =  %d",sum);
+}
 
