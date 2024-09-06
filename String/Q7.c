a2324430942a3aa2c963634707bb4f6cd36288c4
Ritesh777Singh <singhrt456@gmail.com>
2024-09-06 11:19:25 +0530

    Update Q7.c

--- String/Q7.c
+++ String/Q7.c
@@ -1,1 +1,12 @@
+// 7. Write a program in C to copy one string to another string
 
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char str[20];
+    printf("Enter String:");
+    fgets(str,sizeof(str),stdin);
+    char str2[20];
+    strcpy(str2,str);
+    printf("String1 : %sString2: %s",str,str2);
+}
