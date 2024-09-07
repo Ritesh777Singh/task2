04348140ef2786b96bf31eb3e42f54182cc2e866
Ritesh777Singh <singhrt456@gmail.com>
2024-09-07 10:45:34 +0530

    Update Q4.c

--- Q4.c
+++ Q4.c
@@ -1,1 +1,11 @@
+// 4. Find Area of Square formula : a = a2
 
+#include <stdio.h>
+int main() {
+    float a, area;
+    printf("Enter the length of area : ");
+    scanf("%f", &a);
+    area = a * a;
+    printf("The area of the square is: %.2f\n", area);
+    return 0;
+}
