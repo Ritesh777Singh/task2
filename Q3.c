bcd6b8d0be1bff48469ca3fd3433bfb779c043a6
Ritesh777Singh <singhrt456@gmail.com>
2024-09-07 10:40:09 +0530

    Update Q3.c

--- Q3.c
+++ Q3.c
@@ -1,1 +1,23 @@
+//3. WAP to Find Area And Circumference of Circle
 
+# include<stdio.h>
+int main(){
+    float radius;
+    printf("enter radius: ");
+    scanf("%f", &radius); 
+    printf("\n area of the circle is : %.2f", 3.14*radius*radius);
+    printf("\n circumference of the circle is : %.2f", 2*3.14*radius);
+    
+    return 0;
+}
+    
+    
+    
+    
+    
+    
+
+    
+    
+    
+    
