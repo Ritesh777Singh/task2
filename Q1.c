b124be4cfbf6275df20c1bdca0af24032791640e
Ritesh777Singh <singhrt456@gmail.com>
2024-09-07 10:26:39 +0530

    Update Q1.c

--- Q1.c
+++ Q1.c
@@ -1,1 +1,14 @@
+/* 1. Display This Information using printf
+a. Your Name
+b. Your Birth date
+c. Your Age
+d. Your Address */
 
+
+#include <stdio.h>
+int main(){
+    printf("Your Name - ritesh singh \n");
+    printf("Your Birth date - 15/06/2000\n");
+    printf("Your Age - 24\n");
+    printf("Your Address - ashwanik park navagam dindoli surat\n");
+}
