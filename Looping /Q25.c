8266e2a35269a79f4ba76656ac9eb7aa6dad55f8
Ritesh777Singh <singhrt456@gmail.com>
2024-09-07 10:52:22 +0530

    Update Q25.c

--- Looping /Q25.c
+++ Looping /Q25.c
@@ -1,16 +1,12 @@
-//25. (1*1) + (2*2) + (3*3) + (4*4) + (5*5) + ... + (n*n)
+//25. (1*1) + (2*2) + (3*3) + (4*4) + (5*5) + ... + (n*n) 
 
-#include<stdio.h>
-int main(){
-    int num;
-    printf("Enter Number :");
-    scanf("%d",&num);
-    for(int i = 1;i <= num; i++){
-        if(i == num){
-            printf("(%d * %d)",i,i);
-        }
-        else{
-            printf("(%d * %d) + ",i,i);
-        }
+#include <stdio.h>
+int main() {
+    int num, sum = 0;
+    printf("enter the number: ");
+    scanf("%d", &num);
+    for(int a = 1; a <= num; a++){
+        sum += a * a;
     }
+    printf("The sum of series is %d\n", sum);
 }
